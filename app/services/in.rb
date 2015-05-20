5f89bb715a3ac5aa2d68143376c7c7f4674519eb
Victor Campos <victorcampos@ufrj.br>
2015-05-19 22:56:23 -0300

    fixing country wrong column issue

--- app/services/in.rb
+++ app/services/in.rb
@@ -32,27 +32,42 @@ class In
   def find_all_followers(agent, followers_page)
     p @followers.count
 
-    next_page_link = followers_page.link_with(:text => /avançar/)
+    next_page_link = followers_page.link_with(text: /avançar/)
 
     return unless next_page_link
 
     followers_page = agent.click(next_page_link)
 
-    followers_from_page(followers_page)
+    followers_from_page(agent, followers_page)
     find_all_followers(agent, followers_page)
   end
 
-  def followers_from_page(followers_page)
+  def followers_from_page(agent, followers_page)
+    threads = []
     followers_page.search('.feed-item').each do |item|
-      follower = {}
+      threads << Thread.new do
+        follower = {
+          name: item.search('a')[1].text,
+          title: item.search('.title').text,
+          city: locations(item)[0],
+          country: locations(item)[1],
+          contact: contact(agent, item)
+        }
 
-      follower[:name] = item.search('a')[1].text
-      follower[:title] = item.search('.title').text
-      follower[:city] = item.search('.location').text.split(',')[0]
-      follower[:country] = item.search('.location').text.split(',')[1]
-
-      @followers << follower
+        @followers << follower
+      end
     end
+    threads.each(&:join)
+  end
+
+  def contact(agent, item)
+    follower_page = agent.click(item.search('a')[1])
+    follower_page.search('#contact-comments-view').text
+  end
+
+  def locations(item)
+    locations = item.search('.location').text.split(',')
+    locations.length > 1 ? [locations[0], locations[1]] : ['', locations[0]]
   end
 
   def login(login_page, user, password)
